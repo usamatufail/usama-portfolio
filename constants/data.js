0fcaa29788f1cbb08d36ea4e8e8f228b91b5bac8
Usama <miansamusama@gmail.com>
2022-02-15 17:24:32 +0500

    changed images and user data

---
+++ constants/data.js
@@ -0,0 +1,117 @@
+const userData = {
+  githubUsername: 'usamatufail',
+  name: 'Usama Tufail',
+  designation: 'React.js Engineer',
+  avatarUrl: '/avatar.jpg',
+  email: 'samtufail726@gmail.com',
+  phone: '+92 301 6795377',
+  address: 'Lahore, Punjab, Pakistan.',
+  projects: [
+    {
+      title: 'Learnatric',
+      link: 'https://learnatric.com',
+      imgUrl: '/1.png',
+    },
+    {
+      title: 'Neufluence',
+      link: 'https://app.neufluence.com',
+      imgUrl: '/2.png',
+    },
+    {
+      title: 'Yalla Class',
+      link: 'https://yallaclass.com/',
+      imgUrl: '/3.png',
+    },
+    // {
+    //   title: 'CovidRescue',
+    //   link: 'https://covidrescue.co.in',
+    //   imgUrl: '/4.png',
+    // },
+    // {
+    //   title: 'Algochurn',
+    //   link: 'https://algochurn.com',
+    //   imgUrl: '/algochurn.png',
+    // },
+    // {
+    //   title: 'Playground',
+    //   link: 'https://play.placeholdertech.in',
+    //   imgUrl: '/playground.png',
+    // },
+    // {
+    //   title: 'Feedmeback',
+    //   link: 'https://feedmeback.vercel.app',
+    //   imgUrl: '/feedmeback.png',
+    // },
+    // {
+    //   title: 'Devmedium',
+    //   link: 'https://devmedium.vercel.app',
+    //   imgUrl: '/devmedium.png',
+    // },
+  ],
+  about: {
+    title:
+      'I am a React.js specialist and I love to code and convert ideas to reality in order to help humans across the planet.',
+    description: [
+      `I'm a senior React.js Specialist who is working remotely for different people around the globe for 3 years. I have experience working in React.js, Typescript, MongoDB, GraphQL, Redux, Redux-Thunk, Redux-Sagas, Express.js, Node.js, Framer Motion, and many other related technologies.`,
+      `I always use international coding standards and proper linting in my code in order to make my code more accessible to every other developer.`,
+      `My main skills include communication as well. I have good English language speaking and listening skills. So, you don't need to worry about communication problems. Please feel free to host an interview/introductory meeting with me to discuss more about me.`,
+    ],
+    currentProject: 'Tailwind Master Kit',
+    currentProjectUrl: 'https://tailwindmasterkit.com',
+  },
+  experience: [
+    {
+      title: 'Software Developer',
+      company: 'mroads',
+      year: '2021',
+      companyLink: 'https://mroads.com',
+      desc: 'Contributed to our flagship product - Panna - which is an AI powered candidate interviewing product.',
+    },
+    {
+      title: 'Founder and Developer',
+      company: 'TailwindMasterKit',
+      year: '2021',
+      companyLink: 'https://tailwindmasterkit.com',
+      desc: 'Developed a marketplace for tailwind web components (React, HTML and Angular) and templates.',
+    },
+    {
+      title: 'Founder and Developer',
+      company: 'Algochurn',
+      year: '2021',
+      companyLink: 'https://algochurn.com',
+      desc: 'Your last moment revision friend right before a Tech coding interview. Practice problems and get hired at top Product based companies.',
+    },
+    {
+      title: 'Internship',
+      company: 'Intel Corporation',
+      year: '2019',
+      companyLink: 'https://intel.com',
+      desc: 'Developed pothole detection system with OpenCV and computer vision, detects potholes on roads and informs the driver.',
+    },
+    {
+      title: 'Graduation',
+      company: 'UPES, Dehradun.',
+      year: '2019',
+      companyLink: 'https://upes.co.in',
+      desc: "Major in Computer Science and Engineering with a CGPA of 7.0. Nobody asks this but it's okay.",
+    },
+    {
+      title: 'High School',
+      company: 'Delhi Public School, Jaipur.',
+      year: '2015',
+      companyLink: 'https://upes.co.in',
+      desc: 'Subjects being PCM - I barely survived with 93.6% marks aggregate. Flex fridays, fellas.',
+    },
+  ],
+  resumeUrl:
+    'https://drive.google.com/file/d/1xmE3BOmgM7TAOOgVp36xQIQvYDntDYoo/view?usp=sharing',
+  socialLinks: {
+    instagram: 'https://instagram.com/maninthere',
+    twitter: 'https://twitter.com/mannupaaji',
+    linkedin: 'https://linkedin.com/in/manuarora28',
+    github: 'https://github.com/manuarora700',
+    facebook: 'https://facebook.com/',
+  },
+};
+
+export default userData;
